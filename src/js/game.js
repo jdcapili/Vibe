795d6c7331a23244bd6d5cd7f56e6f50d8f29fd2
App Academy Student <aa@Apps-Mac-mini-8.local>
2019-10-16 19:32:31 -0400

    sphere rendering with music. sync in progress

--- src/js/game.js
+++ src/js/game.js
@@ -3,34 +3,64 @@ import Visualizer from './visualizer';
 class Game {
   constructor(){
     this.canvas = document.getElementById("canvas");
-    this.playButton = document.getElementById("play-button")
-    const audio = document.getElementById("audio");
-
+    this.playButton = document.getElementById("play-button");
+    this.stopButton = document.getElementById("stop-button");
+    this.audio = document.getElementById("audio");
     
+
     this.playButton.onclick = () => {
-      
-      audio.src =
-        "assets/Feel So Close (Benny Benassi Remix) (Japan Bonus Track).mp3";
-        this.play();
+      this.play();
+    //   audio.src =
+    //     "assets/Feel So Close (Benny Benassi Remix) (Japan Bonus Track).mp3";
+        
     }
 
+    this.stopButton.onclick = () => this.visualizer.mediaElement.pause();
+    this.visualInit();
+
+        // this.playButton.onclick = () => {
+        //   let sound = new THREE.PositionalAudio(this.visualizer.listener);
+        //   console.log("onclick");
+        //   this.visualizer.audioLoader.load(
+        //     "assets/Feel So Close (Benny Benassi Remix) (Japan Bonus Track).mp3",
+        //     function(buffer) {
+        //       console.log("playing??");
+        //       sound.setBuffer(buffer);
+        //       sound.setVolume(0.5);
+        //       sound.setRefDistance(20);
+        //       sound.play();
+        //     }
+        //   );
+        //   this.visualizer.cube.add(sound);
+        // };
+  }
+
+  visualInit(){
+    // const context = new AudioContext();
+    // let src = context.createMediaElementSource(audio);
+    // const analyser = context.createAnalyser();
+    // src.connect(analyser);
+    // analyser.connect(context.destination);
+    // const bufferLength = analyser.frequencyBinCount;
+    // const dataArray = new Uint8Array(bufferLength);
     
+    this.visualizer = new Visualizer(this.canvas);
+    
+    
+
+
   }
 
   play(){
     
     console.log("playing")
-    const context = new AudioContext();
-    let src = context.createMediaElementSource(audio);
-    const analyser = context.createAnalyser();
-    src.connect(analyser);
-    analyser.connect(context.destination)
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
 
 
-    const visualizer = new Visualizer(analyser, dataArray, this.canvas)
-    audio.play();
+    
+    
+    this.visualizer.mediaElement.play();
+    window.visualizer = this.visualizer;
+    this.visualizer.renderFrame();
   }
 }
 
--- src/js/visualizer.js
+++ src/js/visualizer.js
@@ -1,37 +1,124 @@
-import {THREE} from '../three'; 
+// import { THREE } from '../three';
 
 class Visualizer {
-  constructor(analyser, dataArray, canvas){
-    this.analyser = analyser;
-    this.dataArray = dataArray;
+  constructor(canvas){
+    // this.analyser = analyser;
+    // this.dataArray = dataArray;
     this.canvas = canvas;
-
+    this.colorTicker = 0;
     this.scene = new THREE.Scene();
+
     this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
+      90,
+      (window.innerWidth/2) / (window.innerHeight/2),
       0.1,
       1000
     );
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setSize(this.canvas.width, this.canvas.height);
-    this.canvas.appendChild(this.renderer.domElement);
 
-    this.geometry = new THREE.BoxGeometry(1, 1, 1);
-    this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    this.cube = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.cube);
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setSize(window.innerWidth/2, window.innerHeight/2);
 
-    this.camera.position.z = 5;
+    
+    this.animate = this.animate.bind(this)
 
-    this.bufferLength = this.analyser.frequencyBinCount;
+    
+    this.init();
+
 
 
   }
 
+  animate(){    
+    this.renderer.render(this.scene, this.camera);
+        this.sphere.rotation.x += 0.01;
+        this.sphere.rotation.y += 0.01;
+    window.requestAnimationFrame( this.animate );
+  }
+
+  // switchAttributes(){
+
+  // }
+  init(){
+    const degree = Math.PI/180;
+
+
+
+    this.listener = new THREE.AudioListener();
+    this.camera.add(this.listener);
+    this.audio = new THREE.Audio(this.listener);
+    this.mediaElement = new Audio(
+      "assets/Feel So Close (Benny Benassi Remix) (Japan Bonus Track).mp3"
+    );
+    this.mediaElement.autoplay = false;
+    this.mediaElement.loop = true;
+    this.mediaElement.hasPlaybackControl = true;
+    // this.mediaElement.play();
+    
+    this.audio.setMediaElementSource(this.mediaElement);
+
+    let analyser = new THREE.AudioAnalyser(this.audio, 1024);
+    // this.audioLoader = new THREE.AudioLoader();
+    let size = 15, step = 1;
+    let floor = new THREE.Geometry();
+    let floorMat = new THREE.LineBasicMaterial({color: 'white'});
+
+    for(let i = -size; i <= size; i += step){
+      floor.vertices.push(new THREE.Vector3(-size, -0.04, i));
+      floor.vertices.push(new THREE.Vector3(size, -0.04, i));
+
+      floor.vertices.push(new THREE.Vector3(i, -0.04, -size));
+      floor.vertices.push(new THREE.Vector3(i, -0.04, size));
+    }
+
+    let line = new THREE.Line(floor, floorMat, THREE.LinePieces);
+    this.scene.add(line);
+    
+
+    this.size = 3;
+
+    this.geometry = new THREE.SphereGeometry(this.size, 20, 20);
+    this.material = new THREE.MeshBasicMaterial({
+      vertexColors: THREE.FaceColors
+    });
+    this.sphere = new THREE.Mesh(this.geometry, this.material);
+    this.sphere.position.y = 2.5;
+    this.scene.add(this.sphere);
+
+
+    this.camera.position.z = 10;
+
+    this.camera.position.y = 2;
+    
+    
+  }
+
+
   renderFrame(){
-    requestAnimationFrame(render);
-    this.renderer.render(scene, camera);
+    window.geo = this.geometry;
+    let toggleColor = "positive";
+  
+
+       setInterval(() => {
+          let numSides = this.geometry.faces.length;
+          for (let i = 0; i < numSides; i++) {
+            let vertIndex = this.geometry.faces[i];
+            var color = new THREE.Color(0xffffff);
+            color.setRGB(this.colorTicker, Math.random(1.0), this.colorTicker);
+            vertIndex.color = color;
+          }
+          this.geometry.elementsNeedUpdate = true;
+
+          if(toggleColor === "negative"){ 
+            this.colorTicker -= 0.05
+            if (this.colorTicker <= 0) toggleColor = "positive";
+          }else if (toggleColor === "positive") {
+            this.colorTicker += 0.05;
+            if (this.colorTicker >= 1) toggleColor = "negative";
+          }          
+        },100);
+        
+  this.animate();
+
   }
 }
 
